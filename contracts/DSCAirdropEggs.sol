15ea14ec9308d5480d1d6f07b319761900cab479
Hanul <hanul@hanul.me>
2021-10-05 16:09:20 +0900

    개발 완료

---
+++ contracts/DSCAirdropEggs.sol
@@ -0,0 +1,50 @@
+pragma solidity ^0.5.6;
+
+import "./klaytn-contracts/token/KIP17/KIP17Full.sol";
+import "./klaytn-contracts/token/KIP17/KIP17Mintable.sol";
+import "./klaytn-contracts/token/KIP17/KIP17Burnable.sol";
+import "./klaytn-contracts/token/KIP17/KIP17Pausable.sol";
+import "./klaytn-contracts/ownership/Ownable.sol";
+
+contract DSCAirdropEggs is Ownable, KIP17Full("DSC Airdrop Eggs", "DSCEGGS"), KIP17Mintable, KIP17Burnable, KIP17Pausable {
+
+    function tokenURI(uint256 tokenId) public view returns (string memory) {
+        require(_exists(tokenId), "KIP17Metadata: URI query for nonexistent token");
+        
+        if (tokenId == 0) {
+            return "https://api.dogesound.club/airdropeggs/0";
+        }
+
+        string memory baseURI = "https://api.dogesound.club/airdropeggs/";
+        string memory idstr;
+        
+        uint256 temp = tokenId;
+        uint256 digits;
+        while (temp != 0) {
+            digits += 1;
+            temp /= 10;
+        }
+        bytes memory buffer = new bytes(digits);
+        while (tokenId != 0) {
+            digits -= 1;
+            buffer[digits] = bytes1(uint8(48 + uint256(tokenId % 10)));
+            tokenId /= 10;
+        }
+        idstr = string(buffer);
+
+        return bytes(baseURI).length > 0 ? string(abi.encodePacked(baseURI, idstr)) : "";
+    }
+
+    function massMint(uint256 start, uint256 end) external onlyMinter {
+        for (uint256 i = start; i <= end; i += 1) {
+            mint(msg.sender, i);
+        }
+    }
+
+    function bulkTransfer(address[] calldata tos, uint256[] calldata ids) external {
+        uint256 length = ids.length;
+        for (uint256 i = 0; i < length; i += 1) {
+            transferFrom(msg.sender, tos[i], ids[i]);
+        }
+    }
+}
